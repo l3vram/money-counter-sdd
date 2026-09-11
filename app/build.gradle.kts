674d04cc5780a41aaa9e1cb5b0a2f0090400e018
Marvel Alvarez Rojas <marvel.alvarez@coderio.co>
2026-09-11 12:36:10 -0300

    release: bump to v1.6 (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.moneycounter"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
